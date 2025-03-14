2a21a6aa26a86fa7aee8bfb63fe75d1e5f190a5d
tinamanga <christinamanga28@gmail.com>
2025-03-15 02:17:38 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
